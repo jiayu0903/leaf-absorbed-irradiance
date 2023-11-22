29f59ed1e5b44e11ba659cd42bc00658b84abcab
Zhang,Jiayu <jiayu.zhang@wur.nl>
2023-11-22 20:30:08 +0100

    script cucumber

---
+++ Validation/30cm-ordered light/model.stan
@@ -0,0 +1,103 @@
+functions {
+  int setSpline(real x, real xmax, array[] real values);
+  real getSVal(int idx, real x);
+  
+  real lcheck(real t, array[] real tON, array[] real dt, array[] real Qabs, int N) {
+    for (i in 1:N) {
+      if (t > tON[i] && t < (tON[i] + dt[i]))
+        return Qabs[i];
+    }  
+    return 0.;
+  }
+  
+  real SatVap(real Tc) {
+    return(613.65 * exp(17.502 * Tc / (240.97 + Tc))); // Pa
+  }
+  
+  real LatentHeat(real Tk) {
+    return(1.91846E6 * pow(Tk / (Tk - 33.91), 2)); // J Kg-1
+  }
+  
+  vector derivs(real t, vector y,
+                real gbl, real gsw, array[] real Qabs,
+                array[] real tON, array[] real dt, int N) {
+    
+    real Tair = getSVal(0, t);
+    real RH = getSVal(1, t);
+    real Treflect = getSVal(2, t);
+    real Pa = 101300.;
+    
+    real rho = Pa / (287.058 * (Tair + 273.));
+    real ea = SatVap(Tair) * RH;
+    real SH = 0.622 * ea / (Pa - ea);
+    real Cs = 1005. + 1820. * SH;
+    
+    real SW = lcheck(t, tON, dt, Qabs, N);
+    real LW = 2. * 0.97 * 5.6703E-8 * (pow(Treflect + 273., 4) - pow(y[1] + 273., 4));
+    real R = SW + LW;
+    
+    real C = 2. * rho * Cs * gbl * (y[1] - Tair);
+    
+    real lambda = LatentHeat(y[1] + 273.);
+    real vpd = SatVap(y[1]) - ea;
+    real gtw_leaf = 1. / (1. / gsw + 0.92 / gbl);
+    real lE = lambda * 0.622 * rho / Pa * gtw_leaf * vpd;
+    
+    vector[1] dydt;
+    dydt[1] = (R - C - lE) / 690.;
+    return dydt;
+  }
+}
+
+data {
+  int N1;
+  int N2;
+  int N3;
+  int N4;
+  array[N2] real tON;
+  array[N3] real ts;
+  array[N4] real dt;
+  array[N1] real Tair;
+  array[N1] real RH;
+  array[N1] real Treflect;
+  array[N1] real Tleaf;
+  array[N1] real time;
+  real step;
+  real tmax;
+}
+
+transformed data {
+  int T_count = 0;
+  int R_count = 0;
+  int E_count = 0;
+  
+  T_count = setSpline(step, tmax, Tair);
+  R_count = setSpline(step, tmax, RH);
+  E_count = setSpline(step, tmax, Treflect);
+}
+
+parameters {
+  real<lower=1e-6> gbl;
+  real<lower=1e-6> gsw;
+  array[N2] real<lower=1e-6> Qabs;
+  real<lower=1e-6> sT;
+}
+
+transformed parameters {
+  array[N1] vector[1] mod;
+  real LL = 0.;
+  
+  mod[1,1] = Tleaf[1];
+  mod[2:,] = ode_bdf_tol(derivs, mod[1,], time[1], time[2:], 1e-10, 1e-10, 10000000,
+                         gbl, gsw, Qabs, tON, dt, N2);
+  
+  LL += normal_lpdf(Tleaf | mod[,1], sT);
+}
+
+model {
+  target += normal_lpdf(gbl | 0, 0.005);
+  target += normal_lpdf(gsw | 0, 0.005);
+  target += normal_lpdf(Qabs | 0, 500);
+  target += normal_lpdf(sT | 0, 0.1);
+  target += LL;
+}
---
+++ Validation/30cm-random light/model.r
@@ -0,0 +1,209 @@
+library(ggplot2)
+library(cowplot)
+library(cmdstanr)
+library(posterior)
+library(bayesplot)
+library(readxl)
+color_scheme_set("brightblue")
+
+theme_set(theme_cowplot())
+
+###
+#
+# Warning: this model is only for a transpiring leaf !
+#
+###
+
+# Directory of the data
+dr <- "./cucumber-1030-30cm/"
+date <- "2022-10-30"
+time <- "14:25:00"
+
+# Start time of the temperature records
+t0 <- as.POSIXct(paste(date, time), format="%Y-%m-%d %H:%M:%OS")
+
+# Time points when the light was switched on
+tON <- c("14:25:00",
+         "14:29:00",
+         "14:33:00",
+         "14:37:00",
+         "14:41:00")
+
+# Observed vs. estimated light intensity (same units)
+PAR <- c(128.7322691,
+         111.271802,
+         186.2555652,
+         144.4270982,
+         168.1329206)
+
+# Read and process air temperature data
+dA <- read.csv(paste0(dr, "AirT.csv"))
+dA$time <- as.POSIXct(paste(date, dA$Time), format="%Y-%m-%d %I:%M:%OS %p")
+ggplot(dA, aes(x=time, y=Temperature)) + geom_point()
+ggplot(dA, aes(x=time, y=Humidity)) + geom_point()
+
+# Read and process leaf temperature data
+dL <- read.csv(paste0(dr, "LeafT.csv"))
+names(dL) <- c("Date", "Time", "Temperature", "Tair","Treflect")
+
+# Parse date string
+dL$ts <- as.POSIXct(paste(date, dL$Date), format="%Y-%m-%d %I:%M:%OS %p")
+ggplot(dL, aes(x=ts, y=Temperature)) + geom_point()
+ggplot(dL, aes(x=ts, y=Tair)) + geom_point()
+
+# Find initial time common to both data sets
+dA$time <- as.numeric(dA$time - t0)
+dL$time <- dL$Time + as.numeric(dL$ts[1] - t0)
+
+dA <- subset(dA, time >= 0)
+dL <- subset(dL, time >= 0)
+
+# Use splines to extract data at common time points
+spl_A <- splinefun(dA$time, dA$Temperature)
+spl_R <- splinefun(dA$time, dA$Humidity)
+spl_L <- splinefun(dL$time, dL$Temperature)
+spl_A2 <- splinefun(dL$time, dL$Tair)
+spl_E <- splinefun(dL$time, dL$Treflect)
+
+fitt <- smooth.spline(dA$time, dA$Humidity, cv=TRUE)
+fitt2 <- smooth.spline(dA$time, dA$Temperature, cv=TRUE)
+fitt3 <- smooth.spline(dL$time, dL$Tair, cv=TRUE)
+fitt4 <- smooth.spline(dL$time, dL$Treflect, cv=TRUE)
+
+fitt$df
+fitt2$df
+fitt3$df
+fitt4$df
+
+# Remove high frequency fluctuations
+sspl_R <- smooth.spline(dA$time, dA$Humidity, df=62)
+sspl_A <- smooth.spline(dA$time, dA$Temperature, df=33)
+sspl_A2 <- smooth.spline(dL$time, dL$Tair, df=207)
+sspl_E <- smooth.spline(dL$time, dL$Treflect, df=205)
+
+
+# Check spline function
+plot(Humidity~time, dA, pch=16)
+points(predict(sspl_R, time)$y~time, dA, lty=2, lwd=2, col="red")
+
+plot(Temperature~time, dA, pch=16)
+lines(predict(sspl_A, time)$y~time, dA, lty=2, lwd=2, col="red")
+
+plot(Tair~time, dL, pch=16)
+lines(predict(sspl_A2, time)$y~time, dL, lty=2, lwd=2, col="red")
+
+# New time points
+time <- subset(dL, time < max(dA$time))$time
+
+# Merge the data sets
+d <- data.frame(time=time, Ta=predict(sspl_A, time)$y,
+                Ta2=predict(sspl_A2, time)$y,
+                Treflect=predict(sspl_E, time)$y,
+                Tl=spl_L(time),
+                RH=predict(sspl_R, time)$y)
+
+# Correct for offset between Rotronic sensor and thermal camera temperature
+#d$Tl <- d$Tl - (tail(d$Tl) - tail(d$Ta))
+
+ggplot(d, aes(x=time)) + geom_point(aes(y=Ta)) +
+  geom_point(aes(y=Ta2), col="blue") +
+  geom_point(aes(y=Tl), col="red") + geom_point(aes(y=Treflect), col="green") +ylab("Temperature")
+ggplot(d, aes(x=time, y=Tl - Ta)) + geom_point() + ylab("Tl - Ta")
+
+
+# Function to be passed to the ODE solver
+l <- NULL
+l$N1 <- nrow(d)
+
+# Time light was turned ON
+l$tON <- paste(date, tON)
+l$tON <- as.POSIXct(l$tON, format="%Y-%m-%d %H:%M:%OS")
+l$tON <- as.numeric(l$tON - t0, units="secs")
+l$N2 <- length(l$tON)
+
+l$ts <- diff(c(0, l$tON, max(d$time)))
+l$N3 <- length(l$ts)
+l$dt <- rep(30, length(l$tON))
+l$N4 <- length(l$dt)
+
+l$Tair <- d$Ta
+l$RH <- d$RH / 100
+l$Treflect <- d$Treflect
+l$Tleaf <- d$Tl
+l$time <- d$time
+l$step <- 0.1
+l$tmax <- max(d$time)
+
+###
+# Generate initial parameter values
+##
+init <- function()
+{
+  list(gbl=runif(1,0.01,0.03),
+       gsw=runif(1,0.001,0.003),
+       Qabs=runif(length(l$tON), 20, 200),
+       sT=runif(1,0.2,0.3))
+}
+
+# Compile the bayesian model
+file <- file.path(getwd(), "model.stan")
+header <- file.path(getwd(), "user_header.hpp")
+mod <- cmdstan_model(file,
+                     cpp_options=list(USER_HEADER=header),
+                     stanc_options = list("allow-undefined"))
+
+# Fit the model to observed data
+fit <- mod$sample(
+  data = l,
+  chains = 4,
+  parallel_chains = 4,
+  refresh = 10,
+  init=init,
+  iter_warmup = 500,
+  iter_sampling = 500,
+  adapt_delta=0.7,
+  save_warmup = T,
+  output_dir=getwd(),
+  metric = "dense_e",
+  step_size=0.0001
+)
+
+# See if the model converged properly
+fit$cmdstan_diagnose()
+fit$save_object(file = "fit.RDS")
+fit <- readRDS("fit.RDS")
+
+# Diagnostic graphs
+np <- names(init())
+fs <- fit$summary(np)
+mcmc_hist(fit$draws(np))
+mcmc_trace(fit$draws(np), n_warmup=500)
+mcmc_pairs(fit$draws(np))
+
+png("Kin.png", width=1280, height=960, res=200)
+par(mar=c(5,5,2,2))
+mod <- matrix(matrix(fit$draws("mod")[,1,], nrow=500)[500,], ncol=1)
+Tl_mod <- mod[,1]
+plot(Tl~time, d, pch=16, 
+     xlab="Time (s)",
+     ylab=expression("T ("*degree*"C)"))
+points(Tl_mod~time, d, type="l", col="red", lty=2, lwd=2)
+dev.off()
+
+png("PAR.png", width=1280, height=960, res=200)
+par(mar=c(5,5,2,2))
+Q_mod <- fs$mean[3:(2+length(l$tON))]
+plot(Q_mod~PAR, d, pch=16, xlim=c(0,300), ylim=c(0,300),
+     xlab=expression("PAR obs. (W m"^-2*")"),
+     ylab=expression("PAR mod. (W m"^-2*")"))
+abline(lm(Q_mod~PAR-1, d), lwd=2, lty=2, col="red")
+abline(0, 1, lwd=2, lty=2, col="blue")
+dev.off()
+
+dl <- data.frame(PAR_obs=PAR, PAR_mod=Q_mod)
+write.table(dl, "Light.csv", row.names=F, col.names=F, sep=",")
+
+write.table(Tl_mod, "Temperature.csv", row.names=F, col.names=F, sep=",")
+write.table(d$Tl, "Temperature-Obs.csv", row.names=F, col.names=F, sep=",")
+
+
